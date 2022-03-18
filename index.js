cb62e035099ac63eb0e5a6e502f9c91be2359255
unknown <aluno@L4M04.IPIRANGA.FATEC>
2022-03-18 17:19:09 -0300

    Exemplo async

--- index.js
+++ index.js
@@ -1,40 +1,70 @@
-require('dotenv').config()
-const axios = require ('axios')
+// require('dotenv').config()
+// const axios = require ('axios')
 
-//operador de desestruturação
-const { APPID, PROTOCOL, BASE_URL, UNITS, LANGUAGE, Q, CNT } = process.env
+// //operador de desestruturação
+// const { APPID, PROTOCOL, BASE_URL, UNITS, LANGUAGE, Q, CNT } = process.env
 
-const url = `${PROTOCOL}://${BASE_URL}?q=${Q}&appid=${APPID}&lang=${LANGUAGE}&units=${UNITS}&cnt=${CNT}`
+// const url = `${PROTOCOL}://${BASE_URL}?q=${Q}&appid=${APPID}&lang=${LANGUAGE}&units=${UNITS}&cnt=${CNT}`
 
-//console.log(url)
+// //console.log(url)
 
-//processamento sincrono - bloqueante 
-// const resultado = axios.get(url)
+// //processamento sincrono - bloqueante 
+// // const resultado = axios.get(url)
 
+// // axios.get(url)
+// // .then(res => console.log(res.data))
+
+
+//Parecido com inferno de callbacks
 // axios.get(url)
-// .then(res => console.log(res.data))
+// .then(res => {
+//     //console.log(res.data)
+//     return res.data
+// })
+// .then(res => {
+//     console.log(res)
+//     return res
+// })
+// .then(res => {
+//     console.log(res.cnt)
+//     return res
+// })
+// .then(res => {
+//     return res.list
+// })
+// .then(res => {
+//     res.forEach(element => {
+//         console.log("temp_min: " + element.main.temp_min + " temp_max: " + element.main.temp_max)
+//         console.log("description: " + element.weather[0].description + "\n") //element.weather[0] pq o weather está associado a um vetor 
+//     });
+// })
 
+//promises: ECMAScript 2015
+//async/await: ECMAScript 2017
+// function hello (nome){
+//     return `Hello, ${nome}`
+// }
 
-axios.get(url)
-.then(res => {
-    //console.log(res.data)
-    return res.data
-})
-.then(res => {
-    console.log(res)
-    return res
-})
-.then(res => {
-    console.log(res.cnt)
-    return res
-})
-.then(res => {
-    return res.list
-})
-.then(res => {
-    res.forEach(element => {
-        console.log("temp_min: " + element.main.temp_min + " temp_max: " + element.main.temp_max)
-        console.log("description: " + element.weather[0].description + "\n") //element.weather[0] pq o weather está associado a um vetor 
-    });
-})
+// const resultado = hello("Ana")
+// console.log("Até mais!")
 
+//transformando em promise
+// function hello (nome){
+//     return new Promise(
+//         function (resolve, reject){
+//             resolve(`Hello, ${nome}`)
+//     })
+// }
+
+// const resultado = hello("Ana")
+// resultado.then(res => console.log(res))
+
+//Sem fazer manualmente
+//Função assincrona opera devolvendo uma promise 
+//Função async respeita o metodo aberto-fechado 
+async function hello (nome){
+    return `Hello, ${nome}`
+}
+
+const resultado = hello("Ana")
+resultado.then(res => console.log(res))
